0752d7586963b5de09ecb892ec8fde50069ffb71
Tyler Damme <tyler.damme@gmail.com>
2019-01-17 19:05:42 -0600

    generated api service, added to list of providers, imported httpclient to service.ts, and setup CRUD methods

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { post } from 'selenium-webdriver/http';
+import { updateBinding } from '@angular/core/src/render3/instructions';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  API_URL: string = 'http://localhost:3000/';
+
+  constructor(public http: HttpClient) { }
+}
+
+  // read method
+  public get(path) {
+    var endpoint - this.API_URL + path;
+    return this.http.get(endpoint);
+  }
+
+  // create method
+  public post(path:string,body:any) {
+    var endpoint = this.API_URL + path;
+    return this.http.post(endpoint,body);
+  }
+
+  // delete method
+  public delete(path:string) {
+    var endpoint = this.API_URL + path;
+    return this.http.delete(endpoint);
+  }
+
+  // update method
+  public update(path:string, body:any) {
+    var endpoint = this.API_URL + path;
+    return this.http.put(endpoint,body);
+  }
