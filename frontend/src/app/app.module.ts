0662f49b2af90153d5b2d80177d648e9e052adfe
Tyler Damme <tyler.damme@gmail.com>
2019-01-17 19:23:56 -0600

    Enabled CORS in Rails 5

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { OrderAddComponent } from './order-add/order-add.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    OrderAddComponent,
+    CustomerAddComponent,
+    ProductListComponent,
+    OrderListComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule
