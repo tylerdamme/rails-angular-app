08e1c4eff0e5f4030ac68258abfed22927894824
Tyler Damme <tyler.damme@gmail.com>
2019-01-17 18:48:47 -0600

    Added sample data in seeds file, installed Angular into Frontend folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([
+  {name: 'Product Number 1'},
+  {name: 'Product Number 2'},
+  {name: 'Product Number 3'},
+  {name: 'Product Number 4'},
+  {name: 'Product Number 5'}
+])
+
+Customer.create([
+  {name: 'Customer Number 1'},
+  {name: 'Customer Number 2'},
+  {name: 'Customer Number 3'},
+  {name: 'Customer Number 4'},
+  {name: 'Customer Number 5'}
+])
+
+Order.create([
+  {reference: 'Order001'},
+  {reference: 'Order002'},
+  {reference: 'Order003'},
+  {reference: 'Order004'},
+  {reference: 'Order005'}
+])
