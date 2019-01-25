5a26aab106cc60b73babf7eb955b5922ed1201cf
Tyler Damme <tyler.damme@gmail.com>
2019-01-24 19:15:59 -0600

    product list importing and CRUD

--- frontend/src/app/product-list/product-list.component.ts
+++ frontend/src/app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  public columns = ['id', 'name'];
+  public rows : Array<Product>;
+
+  constructor(public apiService: ApiService, public router : Router) { }
+
+  public delete(id:string) {
+    console.log("delete: " + id);
+    var path = 'products/' + id;
+    this.apiService.delete(path).subscribe((r) => {
+      this.rows = this.rows.filter((p,i) => {
+        if (Number(id) === p.id)
+        {
+          return false;
+        }
+        return true;
+      },this.rows)
+    });
+  }
+
+  public update(id:string) {
+    console.log("update: " + id );
+    this.router.navigateByUrl('/products/add/' + id);
+  }
 
   ngOnInit() {
+    this.apiService.get("products").subscribe((data : Product[]) => {console.log(data);
+    this.rows = data;
+  });
   }
 
 }
