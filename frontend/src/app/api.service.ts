a9caed72456df6f4836dfe615e2b6a218bd3ae2c
Tyler Damme <tyler.damme@gmail.com>
2019-01-24 18:43:05 -0600

    app module, imported routermodule, imported path and components. Fixed typo in service CRUD methods

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -9,28 +9,29 @@ export class ApiService {
   API_URL: string = "http://localhost:3000/";
 
   constructor(public http: HttpClient) { }
-};
 
   // read method
   public get(path) {
-    var endpoint - this.API_URL + path;
+    var endpoint = this.API_URL + path;
     return this.http.get(endpoint);
-  };
+  }
 
   // create method
-  public post(path: string, body: any) {
+  public post(path:string,body:any) {
     var endpoint = this.API_URL + path;
     return this.http.post(endpoint,body);
-  };
+  }
 
   // delete method
   public delete(path:string) {
     var endpoint = this.API_URL + path;
     return this.http.delete(endpoint);
-  };
+  }
 
   // update method
   public update(path:string,body:any) {
     var endpoint = this.API_URL + path;
     return this.http.put(endpoint,body);
-  };
+  }
+
+};
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
 import { ProductAddComponent } from './product-add/product-add.component';
@@ -14,14 +14,52 @@ import { CustomerListComponent } from './customer-list/customer-list.component';
   declarations: [
     AppComponent,
     ProductAddComponent,
+    ProductListComponent,
     OrderAddComponent,
     CustomerAddComponent,
-    ProductListComponent,
     OrderListComponent,
     CustomerListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'products',
+        component: ProductListComponent
+      },
+      {
+        path: 'orders',
+        component: OrderListComponent
+      },
+      {
+        path: 'customers',
+        component: CustomerListComponent
+      },
+      {
+        path: 'products/add',
+        component: ProductAddComponent
+      },
+      {
+        path: 'products/add/:id',
+        component: ProductAddComponent
+      },
+      {
+        path: 'orders/add',
+        component: OrderAddComponent
+      },
+      {
+        path: 'orders/add/:id',
+        component: OrderAddComponent
+      },
+      {
+        path: 'customers/add',
+        component: CustomerAddComponent
+      },
+      {
+        path: 'customers/add/:id',
+        component: CustomerAddComponent
+      },
+    ]),
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
