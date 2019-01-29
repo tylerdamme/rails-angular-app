d987ce487ee9e78f5b58386ada70ba6de5430eae
Tyler Damme <tyler.damme@gmail.com>
2019-01-28 19:49:50 -0600

    customer-list and customer-add components finished

--- frontend/src/app/customer-add/customer-add.component.ts
+++ frontend/src/app/customer-add/customer-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Customer } from '../customer';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-customer-add',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerAddComponent implements OnInit {
 
-  constructor() { }
+  public customer: Customer = new Customer();
+
+  constructor(private router: Router, public apiService: ApiService, public acRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.acRoute.params.subscribe((data : any) => {
+      console.log(data.id);
+      if (data && data.id) {
+        this.apiService.get("customers/" + data.id).subscribe((data: Customer) => {
+          this.customer = data;
+        });
+      } else {
+        this.customer = new Customer();
+      }
+    })
+  }
+
+  public onSubmit() {
+    console.log("Adding a customer: " + this.customer.name);
+    if (this.customer.id) {
+      this.apiService.update("customers/" + this.customer.id, this.customer).subscribe((r) => {
+        console.log(r);
+        alert("Customer updated!");
+        this.router.navigateByUrl("/customers");
+      });
+    } 
+    else {
+      this.apiService.post("customers", this.customer).subscribe((r) => {
+        console.log(r);
+        this.customer = new Customer();
+        alert("Customer Successfully Added!");
+        this.router.navigateByUrl("/customers");
+      });
+    }
   }
 
 }
--- frontend/src/app/customer-list/customer-list.component.ts
+++ frontend/src/app/customer-list/customer-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Customer } from '../customer';
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  public columns = ['id', 'name'];
+  public rows : Array<Customer>;
+
+  constructor(public apiService: ApiService, public router: Router) { }
+
+  public delete(id:string) {
+    console.log("delete: " + id);
+    var path = 'customers/' + id;
+    this.apiService.delete(path).subscribe((r) => {
+      this.rows = this.rows.filter((p,i) => {
+        if (Number(id) === p.id) {
+          return false;
+        }
+          return true;
+      },this.rows)
+    });
+  }
+
+  public update(id:string) {
+    console.log("update " + id);
+    this.router.navigateByUrl('/customers/add/' + id);
+  }
 
   ngOnInit() {
+    this.apiService.get("customers").subscribe((data: Customer[]) => {
+      console.log(data);
+      this.rows = data;
+    });
   }
 
 }
