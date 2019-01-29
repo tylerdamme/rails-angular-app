f17b14d4e572b29679de2890f6d32e7c2b730a63
Tyler Damme <tyler.damme@gmail.com>
2019-01-28 18:48:30 -0600

    Order-list and Order-add components finished

--- frontend/src/app/order-add/order-add.component.ts
+++ frontend/src/app/order-add/order-add.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../order';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-order-add',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderAddComponent implements OnInit {
 
-  constructor() { }
+  public order: Order = new Order();
+
+  constructor(private router: Router, public apiService: ApiService, public acRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.acRoute.params.subscribe((data: any) => {
+      console.log(data.id);
+      if (data && data.id) {
+        this.apiService.get("orders/" + data.id).subscribe((data: Order) => {
+          this.order = data;
+        });
+      } else {
+        this.order = new Order();
+      }
+    })
+  }
+
+  public onSubmit() {
+    console.log("Adding an order: " + this.order.reference);
+    if (this.order.id) {
+      this.apiService.update("orders/" + this.order.id, this.order).subscribe((r) => {
+        console.log(r);
+        alert("Order updated!");
+        this.router.navigateByUrl("/orders");
+      });
+    } else {
+      this.apiService.post("orders", this.order).subscribe((r) => {
+        console.log(r);
+        this.order = new Order();
+        alert("Order Successfully Added!");
+        this.router.navigateByUrl("/orders");
+      });
+    }
   }
 
 }
--- frontend/src/app/order-list/order-list.component.ts
+++ frontend/src/app/order-list/order-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { Order } from '../order';
 
 @Component({
   selector: 'app-order-list',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderListComponent implements OnInit {
 
-  constructor() { }
+  public columns = ['id', 'reference'];
+  public rows : Array<Order>;
+
+  constructor(public apiService: ApiService, public router : Router) { }
+
+  public delete(id:string) {
+    console.log("delete: " + id);
+    var path = 'orders/' + id;
+    this.apiService.delete(path).subscribe((r) => {
+      this.rows = this.rows.filter((p,i) => {
+        if (Number(id) === p.id) {
+          return false;
+        }
+        return true;
+      },this.rows)
+    });
+  }
+
+  public update(id:string) {
+    console.log("update: " + id);
+    this.router.navigateByUrl('/orders/add/' + id);
+  }
 
   ngOnInit() {
+    this.apiService.get("orders").subscribe((data : Order[]) => {
+      console.log(data);
+      this.rows = data;
+    })
   }
 
 }
